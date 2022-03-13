79babc884e23aad79394c77ae69eb4910645600c
LoZzR <zakariaelhichem@gmail.com>
2022-03-13 11:12:29 +0100

    Correct database settings

--- src/test/java/com/spring/boot/ApplicationTests.java
+++ src/test/java/com/spring/boot/ApplicationTests.java
@@ -1,13 +1,34 @@
 package com.spring.boot;
 
+import com.spring.boot.entities.Person;
+import com.spring.boot.services.PersonService;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ApplicationTests {
 
+	private Logger logger = LoggerFactory.getLogger(ApplicationTests.class);
+
+	@Autowired
+	private PersonService personService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testFindAll() {
+		Set<Person> persons = personService.findAll();
+		logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>testFindAll : {}", persons);
+		assertNotNull(persons);
 	}
 
 }
