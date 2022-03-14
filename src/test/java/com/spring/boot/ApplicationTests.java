60fbee170b6d780d5de29a99c31a170b015a98ab
LoZzR <zakariaelhichem@gmail.com>
2022-03-14 21:37:34 +0100

    Add save person unit test

--- src/test/java/com/spring/boot/ApplicationTests.java
+++ src/test/java/com/spring/boot/ApplicationTests.java
@@ -7,13 +7,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class ApplicationTests {
 
 	private Logger logger = LoggerFactory.getLogger(ApplicationTests.class);
@@ -33,6 +36,17 @@ class ApplicationTests {
 		Set<Person> persons = personService.findAll();
 		logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>testFindAll : {}", persons);
 		assertNotNull(persons);
+		assertEquals(2, persons.size());
+	}
+
+	@Test
+	@Transactional
+	@Rollback//by default, just add for example
+	public void testSavePerson(){
+		Person person = new Person();
+		person.setFirstName("Irene");person.setLastName("Adler");
+		personService.save(person);
+		assertEquals(3, personService.count());
 	}
 
 }
